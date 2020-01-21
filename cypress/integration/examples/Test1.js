4e52ca6971a1b465882d1b30f2dd9e039acc34a4
unknown <baljinder_k11@hotmail.com>
2020-01-20 20:14:03 -0500

    commit with some changes in test1.js

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,12 +6,18 @@ it("First test case", function(){
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
 
-    //cy.get('.product').should('have.length',4)
+    
     cy.get('.product:visible').should('have.length', 4)
 
-    // finding elements in parent child chaining
+    // finding elements in parent child chaining 
+    // counting all the products from list of products
     cy.get('.products').find('.product').should('have.length',4)
+
+    // finding 2nd product from the list of products and clicking on add to cart 
     cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+    
+    // finding a particular product cashews from list of product by dynamically going to each product using array and clicking it 
 
     cy.get('.products').find('.product').each(($e1,index,$list) => {
       
